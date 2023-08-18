74eb16095e92e581ab4ec8747ffbe778613e9647
Shubham Mangukiya <smangukiya2@myseneca.ca>
2023-08-17 23:00:14 -0400

    FP

---
+++ valid.js
@@ -0,0 +1,24 @@
+function validateInput(){
+    var postCode = document.getElementById("Postal").value.trim();
+    var error = document.getElementById('error');
+    let valid = true;
+    let postCodeRep =/[a-zA-Z]\d[a-zA-Z]\s?\d[a-zA-Z]\d/gm;
+    if(!postCodeRep.test(postCode)){
+        error.innerHTML += "Please enter the right Post Code!"
+        valid = false;
+    }
+    return valid;
+}
+
+function handleChangeOther(){
+    const messageEle = document.getElementById("other-input-container");
+    const textBox = document.createElement("input");
+    textBox.classList = "message_text";
+    textBox.name = "message";
+    messageEle.innerHTML = "";
+    messageEle.appendChild(textBox);
+}
+
+
+
+
